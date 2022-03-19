11745c59e632f9d43b986413a72b6a7b38697d87
SOUMABHA <simply.shubham@gmail.com>
2022-03-19 14:07:25 +0530

    first commit

---
+++ src/main/java/com/restapi/oracle/crud/example/SpringBootRestApiOracleApplication.java
@@ -0,0 +1,28 @@
+package com.restapi.oracle.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class SpringBootRestApiOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiOracleApplication.class, args);
+	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*"); 
+            }
+        };
+	}
+
+}
